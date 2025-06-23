4c5d28a833d0772b8f501911b4ff5c01bc518901
Nithish <nithishmani79@gmail.com>
2025-06-23 16:30:56 +0530

    This only shows the variables added

---
+++ scripy.js
@@ -0,0 +1,27 @@
+/* var variable
+
+var course = "FullStack";
+console.log(course);
+var course = "Frontend";
+console.log(course);
+console.log(typeof course);
+
+*/
+
+/* let variable
+
+let course = "Cyber Security";
+console.log(course);
+course = "Tally";
+console.log(course);
+console.log(typeof course);
+
+*/
+
+/* const variable
+
+const mode = "Online only";
+console.log(mode);
+console.log(typeof mode);
+
+*/
