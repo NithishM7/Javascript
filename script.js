7e7f5587f75b842ba3f0b84b985beaf32a4da68c
Nithish <nithishmani79@gmail.com>
2025-06-24 15:52:37 +0530

    Just an Data Types and Operators in Js

---
+++ script.js
@@ -0,0 +1,44 @@
+// IN PRIMITIVE DT there are 7 types
+/*  String
+    Number
+    Boolean
+    undefined
+    null
+    symbol
+    Bigint */
+
+// String 
+
+// There are two ways in creating a string and primitve DT.
+// 1. String Literal Way
+// 2. String Constructor Way
+
+
+// Literal Way
+let cName = "Nithish";
+console.log(cName);
+console.log(typeof cName);
+
+
+// Constructor Way
+let cc = new String ("Summa");
+console.log(cc);
+console.log(typeof cc);
+
+// Number and Boolen both are similar like string 
+
+
+// Undefined
+
+let couseDuration;
+console.log(couseDuration);
+console.log(typeof couseDuration);
+
+// null
+
+let courseDuration = null;
+console.log(courseDuration);
+console.log(typeof courseDuration);
+
+
+
