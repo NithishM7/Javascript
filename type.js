2c26d6612b1858c46162b6d4701769d7f947a6e6
Nithish <nithishmani79@gmail.com>
2025-06-25 15:26:05 +0530

    Today Type Conversion & Coercion

---
+++ type.js
@@ -0,0 +1,84 @@
+/* Converting the numeric string into number 
+    1. Number Constructor
+    2. Modern Js
+    3. parseInt
+    4. parseFloat
+
+Converting the number into string
+    1. String Constructor
+    2. Using toString
+
+Converting any number into boolean*/
+
+//Converting numeric into number
+/*
+let num = "2025.44";
+
+console.log(num);
+console.log(typeof num);
+
+console.log(Number(num));
+console.log(typeof Number(num));
+
+
+// Modern Js
+
+console.log(+num);
+console.log(typeof +num);
+
+// parseInt
+
+console.log(parseInt(num));
+console.log(typeof parseInt(num));
+
+// parseFloat
+
+console.log(parseFloat(num));
+console.log(typeof parseFloat(num));
+*/
+
+//Converting number into string
+/*
+let num=2025;
+console.log(num);
+console.log(typeof num);
+
+// String Constructor
+
+console.log(String(num));
+console.log(typeof String(num));
+
+// Using toString
+
+console.log(num.toString());
+console.log(typeof num.toString());
+*/
+
+// Converting any number into boolean
+
+/* The values which give false only
+        '' -- Empty 
+        0
+        undefined
+        null
+        Nan
+        -0
+*/
+
+
+// Concatination in JS
+/*
+//Normal Concatination
+
+let courseName = "Python Development";
+let courseFee = 30000;
+let trainer = "Nithish";
+let place = "Chennai";
+let Mode = "Both Online and Offline";
+
+console.log("My Name is "+trainer+" and am taking "+courseName+" in "+place+" and the class mode is "+Mode+" and the fees is "+courseFee);
+
+// Templat & String Literal Concatination
+
+console.log(`My Name is ${trainer} and am taking ${courseName} in ${place} and the class mode is ${Mode} and the fees is ${courseFee}`);
+*/
